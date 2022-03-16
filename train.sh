c5fd9e4bec636463eb3e1dade4591a55945eb466
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-16 20:07:16 +0800

    YOLOF101 928 1x

--- train.sh
+++ train.sh
@@ -2,12 +2,12 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolof50-DC5 \
+        -v yolof101 \
         -lr 0.03 \
         -lr_bk 0.01 \
         --batch_size 16 \
         --img_size 928 \
-        --grad_clip_norm -1.0 \
+        --grad_clip_norm 4.0 \
         --num_workers 8 \
         --schedule 1x \
         # --mosaic
