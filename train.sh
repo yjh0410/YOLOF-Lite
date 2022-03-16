b5b351b3f2fce222bbb2d6d28a56328ac48d819c
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-16 17:57:13 +0800

    800 size

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ python train.py \
         -lr 0.03 \
         -lr_bk 0.01 \
         --batch_size 16 \
-        --img_size 928 \
+        --img_size 800 \
         --grad_clip_norm -1.0 \
         --num_workers 8 \
         --schedule 1x \
