6aeb5dba95e55d5f4e0f6aef8e1497feacf11161
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-16 08:56:34 +0800

    size=928

--- train.sh
+++ train.sh
@@ -3,10 +3,10 @@ python train.py \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
         -v yolof50 \
-        -lr 0.12 \
-        -lr_bk 0.04 \
-        --batch_size 64 \
-        --img_size 800 \
+        -lr 0.03 \
+        -lr_bk 0.01 \
+        --batch_size 16 \
+        --img_size 928 \
         --grad_clip_norm -1.0 \
         --num_workers 8 \
         --schedule 1x \
--- train_ddp.sh
+++ train_ddp.sh
@@ -1,11 +1,11 @@
-python -m torch.distributed.run --nproc_per_node=2 train.py \
+python -m torch.distributed.run --nproc_per_node=8 train.py \
                                                     --cuda \
                                                     -dist \
                                                     -d coco \
                                                     --root /mnt/share/ssd2/dataset/ \
                                                     -v yolof50 \
-                                                    -lr 0.03 \
-                                                    -lr_bk 0.01 \
+                                                    -lr 0.12 \
+                                                    -lr_bk 0.04 \
                                                     --batch_size 8 \
                                                     --img_size 800 \
                                                     --grad_clip_norm -1.0 \
