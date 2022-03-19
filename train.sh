9c1157e7d6cb27665273de75f9694dd13189b3a5
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-19 09:11:25 +0800

    train YOLOF53-DC5 3x

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ python train.py \
         -lr 0.03 \
         -lr_bk 0.01 \
         --batch_size 16 \
-        --img_size 928 \
+        --img_size 640 \
         --grad_clip_norm 4.0 \
         --num_workers 8 \
         --schedule 3x \
